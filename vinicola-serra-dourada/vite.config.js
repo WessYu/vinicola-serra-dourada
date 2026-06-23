389f35744e736884926854ba23c78e9dec1fd456
Wesley <wess.c@proton.me>
2026-06-23 07:23:13 -0300

    chore: optimize vite production build

--- vinicola-serra-dourada/vite.config.js
+++ vinicola-serra-dourada/vite.config.js
@@ -3,4 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false,
+    cssCodeSplit: true,
+    chunkSizeWarningLimit: 700,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 })
